e0ef09f1dbdad731e8125c3d948cdac2f539368e
crimx <straybugs@gmail.com>
2021-05-24 16:13:20 +0800

    refactor: export VideoPlayerConfig

--- src/Players/VideoPlayer/index.ts
+++ src/Players/VideoPlayer/index.ts
@@ -2,6 +2,8 @@ import { AtomPlayer } from "../AtomPlayer";
 import { ChromeAtomPlayer } from "./ChromeAtomPlayer";
 import { VideoPlayerConfig } from "./Types";
 
+export type { VideoPlayerConfig } from "./Types";
+
 export const VideoPlayer = function VideoPlayer(config: VideoPlayerConfig): AtomPlayer {
     // @TODO pick player according to platform
     return new ChromeAtomPlayer(config);
