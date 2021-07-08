9b5b9c2270b3f9b603534fd2f25e781d8e69b453
CRIMX <straybugs@gmail.com>
2021-07-08 15:58:24 +0800

    build(webpack): add peer deps external plugin (#14)

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,8 @@
 const { merge } = require("webpack-merge");
 const config = require("./webpack.common.js");
+const PeerDepsExternalsPlugin = require("peer-deps-externals-webpack-plugin");
 
 module.exports = merge(config, {
     mode: "production",
+    plugins: [new PeerDepsExternalsPlugin()],
 });
